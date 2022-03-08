eeff84050aa8d4bba08d7d52ea207cad7c4efc4c
hcipek <40670635+hcipek@users.noreply.github.com>
2022-03-08 19:07:41 +0300

    Update extra_field.py

--- extra_field.py
+++ extra_field.py
@@ -37,56 +37,63 @@ currentEpoch = 0
 #
 exceptionCount = 100
 
+hashDataDict = dict()
+
+def writeToDataframe(data, i):
+    ## GET FIELDS AND ADD THEM TO DATAFRAME ##
+    # Get date of the commit and add to dataframe
+    commit_date = data['commit']['author']['date']
+    df['commit_date'][i] = commit_date
+    
+    # Get user's real name
+    user_name = data['commit']['author']['name']
+    df['user_name'][i] = user_name
+    
+    # Get commit message
+    commit_message = data['commit']['message']
+    df['commit_message'][i] = commit_message
+    
+    # Get user's avatar url
+    avatar_url = data['author']['avatar_url']
+    df['avatar_url'][i] = avatar_url
+
+    # Get user's follower count at commit time
+    p = requests.get(data['author']['followers_url'])
+    follower_count = len(p.json())
+    df['follower_count'][i] = follower_count
 
 for i in range(len(df)):
     # Split github_repo_url
     protocol, _, domain, user, repo = str(df['github_repo_url'][i]).split('/')
     # Get commit hash from dataframe
     commit_hash = str(df['commit_hash'][i]).replace('\n', '')
-
-    # Merge url to send request github api
-    url = protocol + '//' + 'api.' + domain + '/repos/' + user + '/' + repo + '/' + 'commits/' + commit_hash
-    # Send request to api
-    try:
-        r = requests.get(url, headers=header)
-        recievedHeaders = dict(r.headers)
-        remainingLimit = int(recievedHeaders['X-RateLimit-Remaining'])
-        resetEpoch = int(recievedHeaders['X-RateLimit-Reset'])
-        # Dump json
-        data = r.json()
-        ## GET FIELDS AND ADD THEM TO DATAFRAME ##
-        # Get date of the commit and add to dataframe
-        commit_date = data['commit']['author']['date']
-        df['commit_date'][i] = commit_date
-        
-        # Get user's real name
-        user_name = data['commit']['author']['name']
-        df['user_name'][i] = user_name
-        
-        # Get commit message
-        commit_message = data['commit']['message']
-        df['commit_message'][i] = commit_message
-        
-        # Get user's avatar url
-        avatar_url = data['author']['avatar_url']
-        df['avatar_url'][i] = avatar_url
     
-        # Get user's follower count at commit time
-        p = requests.get(data['author']['followers_url'])
-        follower_count = len(p.json())
-        df['follower_count'][i] = follower_count
-        
+    if (commit_hash in hashDataDict):
+        data = hashDataDict[commit_hash]
+        writeToDataframe(data, i)
+    else:        
+        # Merge url to send request github api
+        url = protocol + '//' + 'api.' + domain + '/repos/' + user + '/' + repo + '/' + 'commits/' + commit_hash
+        # Send request to api
+        try:
+            r = requests.get(url, headers=header)
+            recievedHeaders = dict(r.headers)
+            remainingLimit = int(recievedHeaders['X-RateLimit-Remaining'])
+            resetEpoch = int(recievedHeaders['X-RateLimit-Reset'])
+            # Dump json
+            data = r.json()
+            writeToDataframe(data, i)
+        except:
+            writeLog("Exception happened! Current remainingLimit : {0}, resetEpoch : {1}, currentEpoch : {2}".format(remainingLimit, resetEpoch, currentEpoch))
+            writeLog(traceback.format_exc())
+            i -= 1
+            exceptionCount -= 1
+            if (exceptionCount < 0):
+                i = len(df)
         if(remainingLimit == 0):
             # sleep till you get the time limit but check every 5 mins
             while (currentEpoch < resetEpoch):
                 time.sleep(300)
                 currentEpoch = int(time.time())
-    except:
-        writeLog("Exception happened! Current remainingLimit : {0}, resetEpoch : {1}, currentEpoch : {2}".format(remainingLimit, resetEpoch, currentEpoch))
-        writeLog(traceback.format_exc())
-        i -= 1
-        exceptionCount -= 1
-        if (exceptionCount < 0):
-            i = len(df)
 # Export data
 df.to_csv('python_extra.csv', index=False)
